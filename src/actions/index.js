730a05c49d4bd6b17600c25d05e3c83a0c13b4c7
inteligenciafutura <inteligenciafuturatv@gmail.com>
2021-12-30 11:27:52 -0500

    Funciona con multiples reducers

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const increment = (num) => {
+    return {
+        type: 'INCREMENT',
+        payload: num
+    }
+}
+export const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+export const handleLoggin = () => {
+    return{
+        type: "SIGN_IN"
+    }
+}
